27b54eae55b6efb3e2cc4994d11816bbfd6ce859
Mohamed Khalaf <mkhalaf2211@gmail.com>
2022-12-16 23:32:08 +0000

    Refactor code

--- player.rb
+++ player.rb
@@ -1,12 +1,35 @@
 class Player
+  attr_accessor :score, :name
 
-  def initialize 
+  def initialize(name) 
+    @name = name
     @score = 3
   end
 
-  def subtract 
+  def take_life 
     @score -= 1
   end
 
+  def is_dead?
+    @score == 0
+  end
+  
+
+  def new_question
+    q1 = Question.new
+    q1.ask_question(name)
+    answer = gets.chomp.to_i
+
+    if q1.check_answer(answer)
+      puts 'Yes! You are correct.'
+    else
+      puts 'Seriously? No!'
+      take_life
+    end
+
+
+  
+  
+  end
 
 end
